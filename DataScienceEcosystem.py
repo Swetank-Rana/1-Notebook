6458ee64f52ae39ad85eb4d2596fb4989d40060b
Swetank Rana <105987789+Swetank-Rana@users.noreply.github.com>
2023-09-13 17:59:37 +0530

    1st Notebook Assignment
    This is my first Notebook Assignment

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem 
+
+# #### Introduction: 
+# In this notebook, Data Science Tools and Ecosystem are summarized. Here we are going to demonstrate some the basic steps which we will be using trrought the cource.
+
+# #### List of Data Science Languages
+# Some of the popular languages that Data Scientists use are given below:
+# 1. Python
+# 2. C++
+# 3. R
+# 4. Go
+
+# #### List of Dayta Science Libraries
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Scikit Learn
+# 2. Matplotlib
+# 3. Pandas
+
+# #### Data Science Tools
+# Here are three development environment open source tools used in data science.
+# Data Science |
+# -----
+# Jupyter Notebook |
+# RStudio |
+# GitHub |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# 
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+(3*4)+5
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+200/60
+
+
+# **Objectives:**
+# List popular languages for Data Science
+# * __Python__
+# * __R__
+# * __C++__
+# 
+
+# ## Author
+# Swetank Rana
+
+# In[ ]:
+
+
+
+
